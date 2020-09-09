a3225a8fc0eecc4d3543bcbdb7553aa984a83bb4
YutangMoo <yutangmoo@gmail.com>
2020-09-09 19:02:30 +1000

    W1_My First Sketch Update

---
+++ Week_01/MyFirstSketch/MyFirstSketch.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas (500,500);
+strokeWeight(5);
+background (255,0,0);
+}
+
+
+function draw() {
+  fill (0,0,255);
+  rect (100,100,300,300);
+  fill (0,255,0);
+  ellipse (250,250,100,100);
+  line (100,100,400,400);
+  
+}
